0459c261e16ad3ecb82270c55adc6b58b9e3be80
JillKlatt <klatt.jill@gmail.com>
2021-08-20 14:21:02 -0400

    Remove React stuff, add form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from'./Form'
+import Letter from './Letter'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    <Switch>
+      <Route exact path='/form'>
+        <Form />
+      </Route>
+      <Route exact path='/letter'>
+        <Letter />
+      </Route>
+      
+      </Switch>
+      {/* <Form /> */}
+      </Router>
     </div>
   );
 }
