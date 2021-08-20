fcea1772c99c34235d02f90bb80ea1e9d82d1e34
JillKlatt <klatt.jill@gmail.com>
2021-08-20 14:21:57 -0400

    Add date component

---
+++ src/Date.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+import { render } from 'react-dom';
+
+   
+
+class App extends Component {
+
+  constructor() {
+
+   
+
+    var today = new Date(),
+
+    date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+   
+
+    this.state = {
+
+      currentDate: date
+
+    }
+
+  }
+  render() {
+
+    return (
+
+      <div>
+
+        <p>
+
+          { this.state.currentDate }
+
+        </p>
+
+      </div>
+
+    );
+
+  }
+
+} 
+
+export default Date
